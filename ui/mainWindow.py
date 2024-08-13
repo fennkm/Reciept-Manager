d302eba86c7d8f2691241d03cfdf9da41c918cc5
“Fenn <wil.alx.km.gmail.com>
2024-08-13 20:11:31 +0100

    Added custom window title

--- ui/mainWindow.py
+++ ui/mainWindow.py
@@ -177,7 +177,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Kit\'s Awesome Receipt Manager"))
         self.nameLabel.setText(_translate("MainWindow", "Item Name"))
         self.quantityLabel.setText(_translate("MainWindow", "Quantity"))
         self.priceLabel.setText(_translate("MainWindow", "Price"))
